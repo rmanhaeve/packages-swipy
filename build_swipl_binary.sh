081c9acf43c384df7e51bdd91e57e4f4540a41fc
Robin Manhaeve <robin.manhaeve@kuleuven.be>
2024-12-10 16:45:53 +0100

    Update build_swipl_binary.sh

--- build_swipl_binary.sh
+++ build_swipl_binary.sh
@@ -3,7 +3,6 @@ rm -rf swipl-devel
 git clone https://github.com/rmanhaeve/swipl-devel.git
 cd swipl-devel
 git submodule update --init
-cd swipl-devel
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=../bundled_swipl -DSWIPL_PACKAGE_LIST="clib;plunit;sgml;semweb;chr;clpqr;nlp;yaml" -DINSTALL_DOCUMENTATION=OFF -DUSE_GM=OFF ..
