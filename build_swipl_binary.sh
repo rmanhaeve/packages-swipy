643e66f0ad691a5cef20306cc513f45227dcceef
Robin Manhaeve <robin.manhaeve@kuleuven.be>
2024-12-12 17:43:48 +0100

    Install swipl directly into package directory,

--- build_swipl_binary.sh
+++ build_swipl_binary.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-rm -rf swipl-devel
 git clone https://github.com/rmanhaeve/swipl-devel.git
 cd swipl-devel
 git submodule update --init
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=../bundled_swipl -DSWIPL_PACKAGE_LIST="clib;plunit;sgml;semweb;chr;clpqr;nlp;yaml" -DINSTALL_DOCUMENTATION=OFF -DUSE_GM=OFF ..
+cmake -DCMAKE_INSTALL_PREFIX=../../janus/bundled_swipl -DSWIPL_PACKAGE_LIST="clib;plunit;sgml;semweb;chr;clpqr;nlp;yaml" -DINSTALL_DOCUMENTATION=OFF -DUSE_GM=OFF ..
 make
 ctest -j $(nproc) --output-on-failure
 make install
