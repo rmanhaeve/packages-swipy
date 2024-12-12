a5ed4fab9ea80e2f9a7acd0bfe23322b6b6bb6cb
Robin Manhaeve <robin.manhaeve@kuleuven.be>
2024-12-12 21:13:16 +0000

    Revert build flags

--- build_swipl_binary.sh
+++ build_swipl_binary.sh
@@ -4,7 +4,7 @@ cd swipl-devel
 git submodule update --init
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=../../janus/bundled_swipl -DSWIPL_PACKAGE_LIST="clib;plunit;sgml;semweb;chr;clpqr;nlp;yaml" -DINSTALL_DOCUMENTATION=OFF -DUSE_GM=OFF -DSWIPL_INSTALL_IN_LIB=ON -DSTATIC_EXTENSIONS=ON ..
+cmake -DCMAKE_INSTALL_PREFIX=../../janus/bundled_swipl -DSWIPL_PACKAGE_LIST="clib;plunit;sgml;semweb;chr;clpqr;nlp;yaml" -DINSTALL_DOCUMENTATION=OFF -DUSE_GM=OFF ..
 make -j $(nproc) 
 ctest -j $(nproc) --output-on-failure
 make install
