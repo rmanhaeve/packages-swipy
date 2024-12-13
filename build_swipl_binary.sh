54d4518edd38a4678988ea29db04991aca09a82c
Robin Manhaeve <robin.manhaeve@kuleuven.be>
2024-12-13 14:57:13 +0100

    Update build_swipl_binary.sh

--- build_swipl_binary.sh
+++ build_swipl_binary.sh
@@ -4,7 +4,7 @@ cd swipl-devel
 git submodule update --init
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=../../janus/bundled_swipl -DSWIPL_SO_VERSIONS=OFF -DSWIPL_PACKAGE_LIST="clib;plunit;sgml;semweb;chr;clpqr;nlp;yaml" -DINSTALL_DOCUMENTATION=OFF -DUSE_GM=OFF ..
+cmake -DCMAKE_INSTALL_PREFIX=../../janus/bundled_swipl -DZLIB_USE_STATIC_LIBS=ON -DSWIPL_SO_VERSIONS=OFF -DSWIPL_PACKAGE_LIST="clib;plunit;sgml;semweb;chr;clpqr;nlp;yaml" -DINSTALL_DOCUMENTATION=OFF -DUSE_GM=OFF ..
 make -j $(nproc) 
 xctest -j $(nproc) --output-on-failure
 make install
